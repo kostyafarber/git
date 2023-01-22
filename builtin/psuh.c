e3c9f1b293d5888b91014ff00125d18fc46fe9c5
Kostya Farber <kostya.farber@gmail.com>
2023-01-22 09:58:23 +0000

    psuh: display the top of origin/master
    Signed-off-by: Kostya Farber <kostya.farber@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,11 +1,15 @@
 #include "builtin.h"
 #include "config.h"
 #include "wt-status.h"
+#include "commit.h"
+#include "pretty.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
+	/* Print this to stdout*/
 	printf(_("This is kostya's first command yo!\n"));
 
+	/* Print our all cmd args to stdou*/
 	int i;
 
 	printf(Q_("Your args (there is %d):\n",
@@ -17,9 +21,11 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
 		printf("%d: %s\n", i, argv[i]);
 	}
 
+	/* print current working directory */
 	printf(_("Your current working directory: \n<top-level>%s%s\n"),
 	prefix ? "/" : "", prefix ? prefix : "");
 
+	/* print any name as defined by git config*/
 	const char *cfg_name;
 
 	git_config(git_default_config, NULL);
@@ -29,11 +35,22 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
 		printf(_("Your name is: %s\n"), cfg_name);
 	}
 
+	/* print current branch*/
 	struct wt_status status;
 
 	wt_status_prepare(the_repository, &status);
 	git_config(git_default_config, &status);
 
 	printf(_("Your current branch is: %s\n"), status.branch);
+
+	struct commit *c = NULL;
+	struct strbuf commitline = STRBUF_INIT;
+
+	c = lookup_commit_reference_by_name("origin/master");
+
+	if (c != NULL) {
+		pp_commit_easy(CMIT_FMT_ONELINE, c, &commitline);
+		printf(_("Current commit: %s\n"), commitline.buf);
+	}
 	return 0;
 }
